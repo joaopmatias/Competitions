207dfe9b53b81034a1f003a2bf2dd082deab6717
JOAO PEDRO CORREIA MATIAS <jpc294@E1-052190.science.psu.edu>
2017-11-03 02:50:19 -0400

    update

--- leetcode/1-50/15.cpp
+++ leetcode/1-50/15.cpp
@@ -11,37 +11,62 @@ using namespace std;
 bool compare (const void * a, const void * b)
 {
 	return (*(int*)a < *(int*)b );
-}
-
-
-class Solution{
-public:
-	vector<vector<int>> threeSum(vector<int> & nums){
-		vector<vector<int>> ans;
-		vector<int> parts (3);
-		sort(nums.begin(), nums.end());
-
-		int i, j, k, n, jj, ii;
-		for(i = 0; i < nums.size(); i++){
-			j = i + 1;
-			k = nums.size() - 1;
-			while(j < k){
-				if(nums[j] + nums[k] < -nums[i]) j++;
-				else if(nums[j] + nums[k] > -nums[i]) k--;
-				else {
-					parts[0] = nums[i];  
-					parts[1] = nums[j];
-					parts[2] = nums[k];
-					ans.push_back(parts);
-					jj = j;
-					while(jj < k && nums[jj] == nums[j]){
-						jj++;
-					}
-					j = jj;
-				}
-			}
-			for(i; nums[i] == nums[i + 1]; i++);
-		}
-		return ans;
-	}
 };
+
+vector< vector <int> > threeSum(vector<int> & nums){
+	vector< vector <int> > ans;
+	vector<int> parts (3);
+	sort(nums.begin(), nums.end());
+
+	int i, j, k, n, jj, ii;
+	for(i = 0; i < nums.size(); i++){
+		j = i + 1;
+		k = nums.size() - 1;
+		while(j < k){
+			if(nums[j] + nums[k] < -nums[i]) j++;
+			else if(nums[j] + nums[k] > -nums[i]) k--;
+			else {
+				parts[0] = nums[i];  
+				parts[1] = nums[j];
+				parts[2] = nums[k];
+				ans.push_back(parts);
+				jj = j;
+				while(jj < k && nums[jj] == nums[j]){
+					jj++;
+				}
+				j = jj;
+			}
+		}
+		for(i; nums[i] == nums[i + 1]; i++);
+	}
+	return ans;
+};
+
+int main(){
+	int n, i, j, k;
+	cin >> n;
+
+	vector<int>  nums (n);
+	for(i = 0; i < n; i++)
+	{
+		cin >> nums[i];
+	}
+
+	vector< vector< int > > triples;
+	triples = threeSum(nums);
+
+	vector< vector <int> >:: iterator it;
+	vector< int >:: iterator jt;
+	cout << '[';
+	for(it = triples.begin(); it != triples.end(); ++it)
+	{
+		cout << '[';
+		for(jt = (*it).begin(); jt != (*it).end(); ++jt)
+		{
+			cout << *jt << ',';
+		}
+		cout << "],";
+	}
+	cout << ']' << endl;
+	return 0;
+}
