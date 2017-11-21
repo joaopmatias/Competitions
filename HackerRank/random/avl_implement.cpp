1e4bafcb832e070e38e4632596bc116b2ef5b42c
JOAO PEDRO CORREIA MATIAS <jpc294@E1-052190.science.psu.edu>
2017-11-20 20:53:42 -0500

    avl insert

--- HackerRank/random/avl_implement.cpp
+++ HackerRank/random/avl_implement.cpp
@@ -19,6 +19,11 @@ int height(node * root){
     else return root -> height;
 }
 
+int getbalance(node * root){
+    if(root == nullptr) return 0;
+    else return height(root -> left) - height(root -> right);
+}
+
 node * newnode(int value){
     node * ans;
     ans -> value = value;
@@ -35,6 +40,7 @@ node * rotateR(node * root){
     x -> right = root;
     root -> height = max(height(root->right), height(root->left)) + 1;
     x -> height = max(height(x->right), height(x->left)) + 1;
+    x -> parent = root -> parent;
     root-> parent = x;
     if(root -> left != nullptr) root -> left -> parent = root;
     return x;
@@ -46,20 +52,55 @@ node * rotateL(node * root){
     z -> left = root;
     root -> height = max(height(root->right), height(root->left)) + 1;
     z -> height = max(height(z->right), height(z->left)) + 1;
+    z -> parent = root -> parent;
     root->parent = z;
     if(root ->right != nullptr) root -> right -> parent = root;
     return z;
 }
-//YOU ARE HERE
+
 node * insertin(node * root, int value){
     node * noob = newnode(value);
     if(root == nullptr) return noob;
-    node *t = root;
-    while(t)
-
-
+    node *t = root, *u = root;
+    while(u != nullptr){
+        t = u;
+        if(value < u -> value) u = u -> left;
+        else u = u-> right;
+    }
+    if(value < t -> value) t -> left = noob;
+    else t-> right = noob;
+    noob -> parent = t;
+    u = t;
+    t = t -> parent;
+    while(t != nullptr){
+        u = t;
+        if(getbalance(u) > 1){
+            if(value < u -> value) u = rotateR(u);
+            else{//left right
+                u -> left = rotateL(u -> left);
+                u = rotateR(u);
+            }
+        }
+        if(getbalance(u) < -1){
+            if(value >= u -> value) u = rotateL(u);
+            else{//right left
+                u -> right = rotateR(u -> right);
+                u = rotateL(u);
+            }
+        }
+        else u -> height = max(height(u->right), height(u-> left)) + 1;
+        t = t -> parent;
+    }
+    return u;
 }
 
+//YOU ARE HERE
+
+//give smallest term too
+node * erasin(node * root, int value){
+
+    return root;
+}
 
 
 int main(){
