c3dca0eaf65ed5ed76d491918c797a77d95cd946
João Matias <joaomatias@joaomatiaco.lan>
2017-12-28 04:03:06 +0000

    work on BigInt

---
+++ HackerRank/random/biginteger_implement.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+class bigint:\
+public:
+
+//operations
+
+//don't forget print
+
+private:
+vector<int> digits;
+int sign;
+int length;
+
+}; 
+
+int main(){
+    cout << "oi" << endl;
+    return 0;
+}
