ca9f5de903e5f1d56eec5918b414be74067cde0a
João Matias <joaomatias@JoaoMatiaCo.local>
2018-05-07 01:18:19 -0400

    update codejam

--- codejam/round1C_2018/B.cpp
+++ codejam/round1C_2018/B.cpp
@@ -9,6 +9,8 @@
 #include <deque>
 #include <set>
 
+typedef long long ll;
+
 using namespace std;
 
 template <class T>
@@ -17,12 +19,54 @@ bool compare (const T a, const T b) //Use templates
 	return ( a < b );
 }
 
-void solve(){
+int solve(){
+	int N;
+	cin >> N;
 
+	vector<int> sold (N,0);
+	vector<int> probs (N,0);
+	vector<int> sell_these;
+	vector<int> customer;
+	int r, i, j, k, D, prob_now;
+	for(i = 0; i < N; i++){
+		cin >> D;
+		if(D == -1) return 1;
+		customer.resize(0);
+		sell_these.resize(0);
+		prob_now = 2000;
+		srand(73);
 
+		for(j = 0; j < D; j++){
+			cin >> k;
+			probs[k]++;
+			if(sold[k] == 0) customer.push_back(k);
+		}
 
+		for(j = 0; j < customer.size(); j++){
+			k = customer[j];
+			if(probs[k] < prob_now){
+				prob_now = probs[k];
+				sell_these.resize(0);
+				sell_these.push_back(k);
+			}
+			else if(probs[k] == prob_now){
+				sell_these.push_back(k);
+			}
+		}
 
-	return;
+		if(sell_these.empty()){
+			cout << -1 << endl;
+		}
+		else{
+			r = rand();
+			r = r % sell_these.size();
+			k = sell_these[r];
+			sold[k] = 1;
+			cout << k << endl;
+		}
+	}
+
+	return 0;
 }
 
 
@@ -33,16 +77,11 @@ int main()
 	int query, q;
 	cin >> query;
 	for(q = 0; q < query; q++){
-		cout << "Case #" << q+1 << ": ";
-		solve();
+		//cout << "Case #" << q+1 << ": ";
+		if(solve()) return 0;
 
 
-
-
-
-
-
-		cout << endl;
+		//cout << endl;
 	}
 	return 0;
 }
--- codejam/round1C_2018/C.cpp
+++ codejam/round1C_2018/C.cpp
@@ -9,6 +9,8 @@
 #include <deque>
 #include <set>
 
+typedef long long ll;
+
 using namespace std;
 
 template <class T>
@@ -18,10 +20,28 @@ bool compare (const T a, const T b) //Use templates
 }
 
 void solve(){
-
-
-
-
+	int N;
+	cin >> N;
+	vector< ll > tower;
+	ll w, ii, jj;
+	int i, j, k, l, n, m;
+	vector< ll >::iterator it;
+	cin >> w;
+	tower.push_back(w);
+	for(i = 1; i < N; i++){
+		cin >> w;
+		it = upper_bound(tower.begin(), tower.end(), 6 * w);
+		if(it == tower.end()){
+			ii = tower.back() + w;
+			tower.push_back(ii);
+			it = prev(tower.end());
+		}
+		for(it; it != tower.begin(); --it){
+			if(*it > *(prev(it)) + w) *it = *(prev(it)) + w;
+		}
+		if(w < tower[0]) tower[0] = w;
+	}
+	cout << tower.size();
 	return;
 }
 
@@ -35,10 +55,6 @@ int main()
 	for(q = 0; q < query; q++){
 		cout << "Case #" << q+1 << ": ";
 		solve();
-
-
-
-
 
 
 
