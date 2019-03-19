f0492bd3237100dcc2279e100ba8d0e99dea2dcc
joaopmatias <joaopmatias@gmail.com>
2019-03-19 19:21:28 +0000

    round 541

--- codeforces/541_div2/C.cpp
+++ codeforces/541_div2/C.cpp
@@ -17,9 +17,31 @@ bool compare (const T a, const T b) //Use templates
 	return ( a < b );
 }
 
+vector<int> arr;
+
 int main()
 {
+	int n, i, j, k, l, ans=0;
+	cin >> n;
+	for(i=0; i<n; i++){
+		cin >> j;
+		arr.push_back(j);
+	}
 
+	sort(arr.begin(), arr.end());
+
+	l = arr.size();
+
+	for(i=0; i<l; i+=2){
+		cout << arr[i] << " ";
+	}
+
+	if(l%2) l--;
+
+	for(i=l-1; i>0; i-=2){
+		cout << arr[i] << " ";
+	}
+	cout << endl;
 
 	return 0;
 }
--- codeforces/541_div2/D.cpp
+++ codeforces/541_div2/D.cpp
@@ -17,9 +17,73 @@ bool compare (const T a, const T b) //Use templates
 	return ( a < b );
 }
 
+
+bool smaller_consistent(vector<int> c, set<int> jumps, vector<int> smaller){
+	int l, i, j, k;
+	l=smaller.size();
+	auto it=upper_bound(jumps.begin(), jumps.end(), l);
+
+	if(it==jumps.begin()) return true;
+
+	it=prev(it);
+
+	for(i=0; i<(*it); i++){
+		auto itt=lower_bound(smaller.begin(), smaller.end(), c[i]);
+		if(itt==smaller.end() || (*itt)!=c[i]) return false;
+	}
+
+	return true;
+}
+
+
 int main()
 {
+	int n, m, i, j, k, l, smaller, equal;
+	vector<int> c_partial_sort, smaller_vec, equal_vec, perm_latter;
+	set<int> jumps, b_rank_in_c;
+	char c;
+	cin >> n >> m;
 
+	for(i=0; i<m; i++) c_partial_sort.push_back(i+1);
+
+	for(i=0; i<n; i++){
+		smaller=0;
+		equal=0;
+		smaller_vec.resize(0);
+		equal_vec.resize(0);
+		perm_latter.resize(0)
+
+		for(j=0; j<m; j++){
+			cin >> c;
+			if(c=='>'){
+				smaller++;
+				smaller_vec.push_back(j+1);
+			}
+			else if(c=='='){
+				equal=1;
+				equal_vec.push_back(j+1);
+			}
+		}
+
+		if(!smaller_consistent(c_partial_sort, jumps, smaller_vec)){
+			cout << 'No' << endl;
+			return 0;
+		}
+
+		// see vectors in argument by reference
+
+
+
+
+		b_rank_in_c.insert(2*smaller+equal);
+		
+
+
+
+		jumps.insert(smaller);
+
+
+	}
 
 	return 0;
 }
